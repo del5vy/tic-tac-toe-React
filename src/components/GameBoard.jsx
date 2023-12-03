0087017872a46dffd6858b9e631bf8e92425474e
Oyedele Sowale <del5vy@yahoo.com>
2023-12-03 08:37:40 +0100

    added X to the board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react'
 
 const initialGameBoard=[
     [null, null, null],
@@ -7,11 +8,21 @@ const initialGameBoard=[
 ]
 
 const GameBoard = () => {
+    const [gameBoard, setGameBoard]=useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        setGameBoard((prevGameBoard)=>{
+const updatedBoard= [...prevGameBoard.map(innerArray=>[...innerArray])]
+
+updatedBoard[rowIndex][colIndex]='X';
+return updatedBoard
+        })
+    }
   return (
     <ol id='game-board'>
-      {initialGameBoard.map((row, rowIndex)=><li key={rowIndex}>
+      {gameBoard.map((row, rowIndex)=><li key={rowIndex}>
         <ol>
-            {row.map((playerSymbol, colIndex)=><li key={colIndex}><button>{playerSymbol}</button></li>)}
+            {row.map((playerSymbol, colIndex)=><li key={colIndex}><button onClick={()=>handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button></li>)}
         </ol>
       </li> )}
     </ol>
